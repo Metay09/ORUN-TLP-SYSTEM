e4d1605a92a2d0a637518b6145079bd43e37e262
Metay09 <metay0996@gmail.com>
2026-09-16 00:37:34 +0300

    fix: bound M6A sensor shutdown retries

--- firmware/include/accelerometer_config.h
+++ firmware/include/accelerometer_config.h
@@ -14,9 +14,12 @@ constexpr uint32_t kProbeTimeoutMs = 500;
 
 constexpr uint8_t kDetectionMaxAttempts = 3;
 constexpr uint32_t kDetectionRetryBackoffMs = 250;
+constexpr uint8_t kPowerDownMaxAttempts = 3;
 
 static_assert(1000UL % kProbeSampleRateHz == 0,
               "accelerometer sample period must be integral milliseconds");
+static_assert(kPowerDownMaxAttempts > 0,
+              "accelerometer power-down must be attempted at least once");
 
 }  // namespace accelerometer_config
 }  // namespace orun_tlp
