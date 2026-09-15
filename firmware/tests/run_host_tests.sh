75bbee4de2df0ec30bdf9b23d0809fb660fb1d18
Metay09 <metay0996@gmail.com>
2026-09-15 23:05:49 +0300

    test: add M6A accelerometer coverage to host suite

--- firmware/tests/run_host_tests.sh
+++ firmware/tests/run_host_tests.sh
@@ -35,6 +35,12 @@ g++ "${flags[@]}" firmware/tests/b4/test_b4_network.cpp \
   -o "$test_dir/b4_network"
 "$test_dir/b4_network"
 
+g++ -Ifirmware/tests/m6/stubs "${flags[@]}" \
+  firmware/tests/m6/test_m6_accelerometer.cpp \
+  firmware/src/accelerometer_manager.cpp firmware/src/i2c_recovery.cpp \
+  -o "$test_dir/m6_accelerometer"
+"$test_dir/m6_accelerometer"
+
 g++ "${flags[@]}" firmware/tests/m3/test_m3.cpp "${gnss_sources[@]}" \
   -o "$test_dir/m3"
 "$test_dir/m3"
@@ -87,6 +93,7 @@ g++ -Ifirmware/tests/startup/stubs -Ifirmware/tests/r2/stubs \
   -Ifirmware/tests/m4/nrf_stubs "${flags[@]}" -fno-pie -no-pie \
   -Wl,--defsym,__flash_arduino_end=0xED000 \
   firmware/tests/startup/test_startup.cpp "${gnss_sources[@]}" \
+  firmware/src/accelerometer_manager.cpp \
   firmware/src/radio_manager.cpp firmware/src/radio_manager_relay_config.cpp \
   firmware/src/radio_driver_gate.cpp \
   firmware/src/rak_device_identity.cpp firmware/src/legacy_position_mapping.cpp \
