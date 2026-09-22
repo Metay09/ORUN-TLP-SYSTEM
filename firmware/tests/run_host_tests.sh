f355511052d21991d6e028e05d8ccbffcb97b8bd
Metay09 <metay0996@gmail.com>
2026-09-23 02:20:43 +0300

    test: link M7P7G portable runtime into startup integration

--- firmware/tests/run_host_tests.sh
+++ firmware/tests/run_host_tests.sh
@@ -215,6 +215,8 @@ g++ -Ifirmware/tests/startup/stubs -Ifirmware/tests/r2/stubs \
   firmware/src/flash_mutation_gate.cpp \
   firmware/src/config_store.cpp firmware/src/config_format.cpp \
   firmware/src/application_request.cpp \
+  firmware/src/ble_application_transport.cpp \
+  firmware/src/ble_application_handoff.cpp \
   firmware/src/security_store.cpp firmware/src/security_format.cpp \
   firmware/src/position_flow.cpp firmware/src/ble_admission_policy.cpp \
   -o "$test_dir/startup"
